50a19025a647494ff0d0c45c73b81f645854a87a
SnwMds <46784020+SnwMds@users.noreply.github.com>
2022-02-02 16:10:49 -0300

    Minor improvements to PackageUtils

--- app/src/main/java/com/amanoteam/unalix/utilities/PackageUtils.java
+++ app/src/main/java/com/amanoteam/unalix/utilities/PackageUtils.java
@@ -16,11 +16,16 @@ public class PackageUtils {
 
 	private static final String PACKAGE_NAME = "com.amanoteam.unalix";
 
-	private static final String CLEAR_URL_ACTIVITY = String.format("%s.%s", PACKAGE_NAME, "ClearURLActivity");
-	private static final String UNSHORT_URL_ACTIVITY = String.format("%s.%s", PACKAGE_NAME, "UnshortURLActivity");
+	private static final String CLEAR_URL_ACTIVITY = String.format("%s.%s", PACKAGE_NAME, "activities.ClearURLActivity");
+	private static final String UNSHORT_URL_ACTIVITY = String.format("%s.%s", PACKAGE_NAME, "activities.UnshortURLActivity");
 
 	private static final ComponentName CLEAR_URL_COMPONENT = new ComponentName(PACKAGE_NAME, CLEAR_URL_ACTIVITY);
 	private static final ComponentName UNSHORT_URL_COMPONENT = new ComponentName(PACKAGE_NAME, UNSHORT_URL_ACTIVITY);
+
+	private static final ComponentName[] CHOOSER_EXCLUDE_COMPONENTS = {
+		CLEAR_URL_COMPONENT,
+		UNSHORT_URL_COMPONENT
+	};
 
 	public static Intent createChooser(final Context context, final String url, final String action) {
 
@@ -40,35 +45,30 @@ public class PackageUtils {
 				return null;
 		}
 
-		// Set package manager
-		final PackageManager packageManager = context.getPackageManager();
-
-		// Set exclude targets
-		final ArrayList<ComponentName> excludeTargets = new ArrayList<>();
-
-		excludeTargets.add(CLEAR_URL_COMPONENT);
-		excludeTargets.add(UNSHORT_URL_COMPONENT);
-
+		
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
 			final Intent chooser = Intent.createChooser(intent, url);
-			chooser.putExtra(Intent.EXTRA_EXCLUDE_COMPONENTS, excludeTargets.toArray(new ComponentName[0]));
+			chooser.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			chooser.putExtra(Intent.EXTRA_EXCLUDE_COMPONENTS, CHOOSER_EXCLUDE_COMPONENTS);
 
 			return chooser;
 		}
 
+		// Set package manager
+		final PackageManager packageManager = context.getPackageManager();
+
 		final List<Intent> intents = new ArrayList<>();
-		final List<ResolveInfo> intentTargets = packageManager.queryIntentActivities(intent, 0);
+		final List<ResolveInfo> targets = packageManager.queryIntentActivities(intent, 0);
 
-		for (ResolveInfo item : intentTargets) {
-			Intent targetIntent = (Intent) intent.clone();
-
-			String packageName = item.activityInfo.packageName;
-			String activityName = item.activityInfo.name;
+		for (final ResolveInfo target : targets) {
+			final String packageName = target.activityInfo.packageName;
+			final String activityName = target.activityInfo.name;
 
 			if (activityName.equals(CLEAR_URL_ACTIVITY) || activityName.equals(UNSHORT_URL_ACTIVITY)) {
 				continue;
 			}
 
+			final Intent targetIntent = (Intent) intent.clone();
 			targetIntent.setComponent(new ComponentName(packageName, activityName));
 			intents.add(targetIntent);
 		}
